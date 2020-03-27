807b549779fe856132dce0abac01551ee2737d15
JullyLoureiro <juliana.ferreira@soulasalle.com.br>
2020-03-26 21:07:32 -0300

    ajustes

--- src/connection/url.js
+++ src/connection/url.js
@@ -1,3 +1,3 @@
-export const linkapi = 'https://covid19.mathdro.id/api/'
+export const linkapi = 'https://covid19.mathdro.id/api/' //http://coronavirus-tracker-api.herokuapp.com/v2/locations
 
 export default linkapi
--- src/ui/App.js
+++ src/ui/App.js
@@ -3,6 +3,7 @@ import Menu from './Menu'
 import {Grid} from '@material-ui/core'
 import Card from './Card'
 import {api} from '../connection/api'
+import styled from 'styled-components'
 
 export default class App extends React.Component {
   constructor(){
@@ -11,6 +12,8 @@ export default class App extends React.Component {
       showLoading: true
     }
   }
+
+  
 
 componentDidMount = () => {
   this.setState({showLoading: true}, ()=>{
@@ -23,20 +26,58 @@ render(){
         <div className="App">
           <header className="App-header">
             <Menu>
+              <Div>
                 <Grid container spacing={2}>
-                    <Grid item sm={12} md={4}>
+                    <Grid item xs={12} md={4}>
                         <Card classe={'cardCont card'} titulo={'Contaminados'} />
                     </Grid>
-                    <Grid item sm={12} md={4}>
+                    <Grid item xs={12} md={4}>
                         <Card classe={'cardMort card'} titulo={'Mortos'} />
                     </Grid>
-                    <Grid item sm={12} md={4}>
+                    <Grid item xs={12} md={4}>
                         <Card classe={'cardRec card'} titulo={'Recuperados'} />
                     </Grid>
                 </Grid>
+              </Div>
             </Menu>
           </header>
         </div>
       )
   }
 }
+
+
+export const Div = styled.div`
+  .cardCont{
+        padding: 8px 8px  !important;
+        color: #ff6600 !important;
+        border-left: 4px solid #ff6600;
+        border-radius: 5px;
+        min-height: 150px;
+        box-shadow: 0px 0px 15px rgba(174, 180, 185,.3);
+  }
+
+  .cardMort{
+      padding: 8px 16px  !important;
+      color: #ff1a75 !important;
+      border-left: 4px solid #ff1a75;
+      border-radius: 5px;
+      min-height: 150px;
+      box-shadow: 0px 0px 15px rgba(174, 180, 185,.3);
+  }
+
+  .cardRec  {
+    padding: 8px 16px  !important;
+    color: #0066ff !important;
+    border-left: 4px solid #0066ff;
+    border-radius: 5px;
+    min-height: 150px;
+    box-shadow: 0px 0px 15px rgba(174, 180, 185,.3);
+  }
+
+  .card: hover{
+      box-shadow:none;
+  }
+
+`;
+
